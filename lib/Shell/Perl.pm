698ae1b2434b18c7d3d912a0e1a25ba2fb5a5ab9
Adriano Ferreira <a.r.ferreira@gmail.com>
2011-03-10 13:37:26 -0300

    Bump code version - Shell-Perl-v0.0020

--- lib/Shell/Perl.pm
+++ lib/Shell/Perl.pm
@@ -8,7 +8,7 @@ use warnings;
 # /Id: Perl.pm 1131 2007-01-27 17:43:35Z me / # don't erase that for now
 # $Id: /iperl/lib/Shell/Perl.pm 2317 2008-03-09T16:22:00.577930Z a.r.ferreira@gmail.com  $
 
-our $VERSION = '0.0019';
+our $VERSION = '0.0020';
 
 use base qw(Class::Accessor); # soon use base qw(Shell::Base);
 Shell::Perl->mk_accessors(qw( out_type dumper context package term ornaments )); # XXX use_strict
